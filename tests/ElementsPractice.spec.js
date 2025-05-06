db35d12569ca88256e48103481ba809b3f85f4f5
CloaizaF <46464898+CloaizaF@users.noreply.github.com>
2025-05-06 10:01:18 -0500

    Elements validation

--- tests/ElementsPractice.spec.js
+++ tests/ElementsPractice.spec.js
@@ -1,5 +1,19 @@
 const { test, expect } = require('@playwright/test');
 
 test('Elements Validations', async ({ page }) => {
-    
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+    //await page.goto('http://google.com/');
+    //await page.goBack();
+    //await page.goForward();
+
+    // Pop Up Validation
+    await expect(page.locator('#displayed-text')).toBeVisible();
+    await page.locator('#hide-textbox').click();
+    await expect(page.locator('#displayed-text')).toBeHidden();
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator('#confirmbtn').click();
+
+    // Mouse Hover Validation
+    await page.locator('#mousehover').hover();
+
 });
