e2997e06679b0d8ed1ec69b6c749899d883610dd
CloaizaF <46464898+CloaizaF@users.noreply.github.com>
2025-05-14 10:51:05 -0500

    Add test data extraction and test parametrization

--- tests/ClientAppPOM.spec.js
+++ tests/ClientAppPOM.spec.js
@@ -1,29 +1,28 @@
 const { test, expect } = require('@playwright/test');
 const { POManager } = require('../page-objects/POManager');
+const testData = JSON.parse(JSON.stringify(require('../tests/utils/placeorderTestData.json')));
 
-test('Client App Validation', async ({ page }) => {
-    const username = "loaizafcamilo@gmail.com";
-    const password = "testing11%";
-    const productName = 'ZARA COAT 3';
-    const poManager = new POManager(page);
-    await page.goto('https://rahulshettyacademy.com/client');
-    const loginPage = poManager.getLoginPage();
-    await loginPage.validLogin(username, password);
-    const dashboardPage = poManager.getDashboardPage();
-    await dashboardPage.searchProductAddCart(productName);
-    await dashboardPage.navigateToCart();
+for (const data of testData) {
+    test(`Client App Validation for ${data.productName}`, async ({ page }) => {
+        const poManager = new POManager(page);
+        await page.goto('https://rahulshettyacademy.com/client');
+        const loginPage = poManager.getLoginPage();
+        await loginPage.validLogin(data.username, data.password);
+        const dashboardPage = poManager.getDashboardPage();
+        await dashboardPage.searchProductAddCart(data.productName);
+        await dashboardPage.navigateToCart();
 
-    const cartPage = poManager.getCartPage();
-    await cartPage.VerifyProductIsDisplayed(productName);
-    await cartPage.Checkout();
+        const cartPage = poManager.getCartPage();
+        await cartPage.VerifyProductIsDisplayed(data.productName);
+        await cartPage.Checkout();
 
-    const ordersReviewPage = poManager.getOrdersReviewPage();
-    await ordersReviewPage.searchCountryAndSelect("ind", "India");
-    const orderId = await ordersReviewPage.SubmitAndGetOrderId();
-    console.log(orderId);
-    await dashboardPage.navigateToOrders();
-    const ordersHistoryPage = poManager.getOrdersHistoryPage();
-    await ordersHistoryPage.searchOrderAndSelect(orderId);
-    expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
-});
-
+        const ordersReviewPage = poManager.getOrdersReviewPage();
+        await ordersReviewPage.searchCountryAndSelect("ind", "India");
+        const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+        console.log(orderId);
+        await dashboardPage.navigateToOrders();
+        const ordersHistoryPage = poManager.getOrdersHistoryPage();
+        await ordersHistoryPage.searchOrderAndSelect(orderId);
+        expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
+    });
+}
